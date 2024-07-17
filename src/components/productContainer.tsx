911be5b7533674cb2d5690db8f4536de7c7c32bd
Pratiba Thiyagarajan <pratiba_thiyagarajan@thbs.india.com>
2024-07-17 10:33:24 +0530

    Product Container Code

---
+++ src/components/productContainer.tsx
@@ -0,0 +1,65 @@
+import React, {useState, useEffect } from 'react'
+import './productContainer.css'
+
+  interface Product {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+  }
+
+const ProductContainer: React.FC = () => {
+  const [products, setProducts] = useState([]);
+
+
+useEffect(() => {
+
+const fetchProducts = async() =>{
+
+  try {
+    const response =await fetch('https://fakestoreapi.com/products');
+    if(!response.ok){
+      throw new Error('Failed to fetch data');
+    }
+    const data = await response.json();
+    setProducts(data);
+
+  }
+
+  catch(error) {
+console.error('Error fetching products',error);  }
+}
+// };
+
+fetchProducts();
+
+},[]);
+
+return (
+    <div className="container">
+<div className=" row"> 
+
+{ products.map((product) => (
+  // <div > 
+<div className="col-3  gy-3 " key ={product.id}>  
+    <div className="card shadow" style={{width: '15rem' , height:'380px'}}>
+  <img src = {product.image} style={{ height:'120px'}} className="card-img-top" alt= {product.title} />
+  <div className="card-body" style={{width:'200px', marginLeft:'0px'}}>
+    <h6 className="card-title"> {product.title}</h6>
+    <p className="card-text">${product.price}</p>
+    <a href="#" className="btn btn-primary">Add to cart</a>
+  </div>
+  </div>
+</div>
+// </div>
+))}
+
+
+</div>
+</div>
+  );
+};
+
+export default ProductContainer;
