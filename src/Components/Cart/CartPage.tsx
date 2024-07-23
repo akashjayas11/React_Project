ba605cf39e613fae0008c7106f1633367e40c97d
Pratiba Thiyagarajan <pratiba_thiyagarajan@thbs.india.com>
2024-07-23 12:54:32 +0530

    css abd  qauanatity increase by pratiba

--- src/Components/Cart/CartPage.tsx
+++ src/Components/Cart/CartPage.tsx
@@ -3,20 +3,44 @@ import { useCart } from '../contexts/CartContext';
 import './CartPage.css'
 
 const CartPage: React.FC = () => {
-  const { cartItems, removeFromCart } = useCart();
-
+  const { cartItems, removeFromCart,addToCart } = useCart();
+  // const {cartItems, setCartItems} = useCart();
   return (
     <div className='Cart_page'>
-      <h2>Cart</h2>
-      <ul>
-        {cartItems.map((item) => (
-          <li key={item.id}>
+      
 
-            <img src={item.image} className='imageContain' />-{item.title} - ${item.price} - Quantity: {item.quantity}
-            <button onClick={() => removeFromCart(item.id)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+<table className="table table-hover" style={{width: '60%'}}>
+  <thead className="table-dark">
+  <tr>
+      <th scope="col">Item</th>
+      <th scope="col">Image</th>
+      <th scope="col">Name</th>
+      <th scope="col">Rate</th>
+      <th scope="col">Update</th>      
+      <th scope="col">Quantity</th>
+      <th scope="col">Price</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    {cartItems.map((item,index) => (
+      <tr key={item.id}>
+      {/* <th scope="row">1</th> */}
+      <td>{index + 1}</td>
+      <td><img src={item.image} style={{width:'100px',height:'100px'}} className='imageContain' /></td>
+      <td>{item.title}</td>
+      <td>${item.price}</td>
+      <td><button className='btn btn-sm btn-primary' onClick={() => addToCart(item.id)}>+</button>
+      <button className='btn btn-sm btn-danger' onClick={() => removeFromCart(item.id)}>-</button>
+      </td>
+      <td>{item.quantity}</td>
+      <td>${item.price * item.quantity}</td>
+    </tr>
+    ))}
+  </tbody>
+</table>
+
+
     </div>
   );
 };
