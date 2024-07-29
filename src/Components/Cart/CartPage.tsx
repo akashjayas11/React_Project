49f1aaa2efd875d3cf50df96d760ceb576b5d0ab
Pratiba_T <ammuabitarp@gmail.com>
2024-07-29 15:37:19 +0530

    cart precisionns and gap between buttons

--- src/Components/Cart/CartPage.tsx
+++ src/Components/Cart/CartPage.tsx
@@ -1,54 +1,63 @@
 import { useCart } from '../contexts/CartContext';
-import './CartPage.css'
-// import PaymentSummary from './PaymentSummary/PaymentSummary';
+import './CartPage.css';
 
-const CartPage= () => {
-  const { cartItems, removeFromCart,addToCart } = useCart();
-  
+const CartPage = () => {
+  const { cartItems, removeFromCart, addToCart } = useCart();
+
   const calculation = (): number => {
-    return cartItems.reduce((total,item) => total + item.price * item.quantity , 0);
-   };
+    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  };
 
   return (
     <div className='Cart_page'>
+      <table className="table table-hover">
+        <thead className="table-dark">
+          <tr>
+            <th scope="col">Item</th>
+            <th scope="col">Image</th>
+            <th scope="col">Name</th>
+            <th scope="col">Rate</th>
+            <th scope="col">Update</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map((item, index) => {
+            const formattedPrice = item.price.toFixed(2);
+            const formattedTotal = (item.price * item.quantity).toFixed(2);
 
-<table className="table table-hover" >
-  <thead className="table-dark">
-  <tr>
-      <th scope="col">Item</th>
-      <th scope="col">Image</th>
-      <th scope="col">Name</th>
-      <th scope="col">Rate</th>
-      <th scope="col">Update</th>      
-      <th scope="col">Quantity</th>
-      <th scope="col">Price</th>
-
-    </tr>
-  </thead>
-  <tbody>
-    {cartItems.map((item,index) => (
-      <tr key={item.id}>
-      {/* <th scope="row">1</th> */}
-      <td>{index + 1}</td>
-      <td><img src={item.image} style={{width:'100px',height:'100px'}} className='imageContain' /></td>
-      <td>{item.title}</td>
-      <td>${item.price}</td>
-      <td><button className='btn btn-sm btn-primary' onClick={() => addToCart(item)}>+</button>
-      <button className='btn btn-sm btn-danger' onClick={() => removeFromCart(item.id)}>-</button>
-      </td>
-      <td>{item.quantity}</td>
-      <td>${item.price * item.quantity}</td>
-      </tr>
-      
- ))}
-       <tr className='table-warning'>
-        <th colspan="6" className="text-right"> Total cost : </th>
-      <th>${calculation()}</th>
-     </tr>
-  </tbody>
-</table>
-
-      {/* <PaymentSummary /> */}
+            return (
+              <tr key={item.id}>
+                <td>{index + 1}</td>
+                <td>
+                  <img
+                    src={item.image}
+                    style={{ width: '100px', height: '100px' }}
+                    className='imageContain'
+                    alt={item.title}
+                  />
+                </td>
+                <td>{item.title}</td>
+                <td>${formattedPrice}</td>
+                <td>
+                  <button
+                    className='btn btn-sm btn-primary me-2' onClick={() => addToCart(item)}>+</button>
+                    
+                  <button
+                    className='btn btn-sm btn-danger'onClick={() => removeFromCart(item)}>-</button>
+                </td>
+                <td>{item.quantity}</td>
+                <td>${formattedTotal}</td>
+              </tr>
+            );
+          })}
+          <tr className='table-warning'>
+            <th colSpan="6" className="text-right">Total cost:</th>
+            <th>${calculation().toFixed(2)}</th>
+          </tr>
+        </tbody>
+      </table>
 
     </div>
   );
